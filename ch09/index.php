15356cc2fd07b9142a0556717eb43f1050e377f3
Jonathan Lee <j47lee@gmail.com>
2015-12-08 11:46:09 -0800

    php mysql

---
+++ ch09/index.php
@@ -0,0 +1,13 @@
+<?php
+  $page_title = 'Welcome to the site!';
+  include ('./includes/header.html');
+?>
+
+<h1 id="mainhead">Header H1</h1>
+<p>Main page content goes here</p>
+<h2>Header H2</h2>
+<p>Main page content goes here</p>
+
+<?php
+  include ('./includes/footer.html');
+?>
